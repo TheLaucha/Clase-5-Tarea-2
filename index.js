90af3eea24f7a68c21065f82dc8be972f38168a5
TheLaucha <47804695+TheLaucha@users.noreply.github.com>
2020-02-11 23:03:15 -0300

    Agregue lineas para que la primer letra de cada nombre este en mayusculas

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ $botonAccion.onclick = function(){
 	const apellido = document.querySelector("#apellido").value;
 	const edad = document.querySelector("#edad").value;
 	const titulo = document.querySelector("h1");
-	document.querySelector("h1").innerText = ("Bienvenido! " + primerNombre);
-	document.querySelector("#campo").innerText = (`Nombre: ${primerNombre} \n Segundo nombre: ${segundoNombre} \n Apellido: ${apellido} \n Edad: ${edad} `);
+	const primerNombreMayus = primerNombre.charAt(0).toUpperCase() + primerNombre.slice(1);
+	const segundoNombreMayus = segundoNombre.charAt(0).toUpperCase() + segundoNombre.slice(1);
+	const apellidoMayus = apellido.charAt(0).toUpperCase() + apellido.slice(1);
+	document.querySelector("h1").innerText = ("Bienvenido! " + primerNombreMayus);
+	document.querySelector("#campo").innerText = (`Nombre: ${primerNombreMayus} \n Segundo nombre: ${segundoNombreMayus} \n Apellido: ${apellidoMayus} \n Edad: ${edad} `);
 	document.querySelector("#campo").style.padding="10px";
 	return false;
 }
